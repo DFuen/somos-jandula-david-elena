00980de52fcfd36918b592b6ea96e6e50e5e32c5
DFuen <dfuemar606@g.educaand.es>
2026-02-24 20:47:44 +0100

    ññ

--- Reaktor_Redes-main/src/main/java/Reaktor_redes_main/dto/EstadoDTO.java
+++ Reaktor_Redes-main/src/main/java/Reaktor_redes_main/dto/EstadoDTO.java
@@ -3,38 +3,40 @@ package Reaktor_redes_main.dto;
 public class EstadoDTO {
     private String nombreRed;
     private String estado = "SIN_INFORMACION";
-    private String fecha_hora;
+    private String fecha;
+    private String hora;
 
-    public EstadoDTO() {
-    }
-
-    public EstadoDTO(String nombreRed, String estado, String fecha_hora) {
+  public EstadoDTO(String nombreRed, String estado, String fecha, String hora) {
         this.nombreRed = nombreRed;
         this.estado = estado;
-        this.fecha_hora = fecha_hora;
-    }
+        this.fecha = fecha;
+  }
+  public EstadoDTO(){
+  } 
+  public String getEstado() {
+      return estado;
+  }
+  public void setEstado(String estado) {
+      this.estado = estado;
+  }
 
-    public String getFecha_hora() {
-        return fecha_hora;
-    }
-
-    public void setFecha_hora(String fecha_hora) {
-        this.fecha_hora = fecha_hora;
-    }
-
-    public String getEstado() {
-        return estado;
-    }
-
-    public void setEstado(String estado) {
-        this.estado = estado;
-    }
-
-    public String getNombreRed() {
-        return nombreRed;
-    }
-
-    public void setNombreRed(String nombreRed) {
-        this.nombreRed = nombreRed;
-    }
+  public String getFecha() {
+      return fecha;
+  } 
+  public void setFecha(String fecha) {
+      this.fecha = fecha;
+  }
+  public void setNombreRed(String nombreRed) {
+      this.nombreRed = nombreRed;
+  }
+  public String getNombreRed() {
+      return nombreRed;
+  }
+  public String getHora() {
+      return hora;
+  }
+  public void setHora(String hora) {
+      this.hora = hora;
+  }
 }
+
